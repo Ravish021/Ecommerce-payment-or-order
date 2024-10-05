87f0335bcba511e4325f955bd2b37eb435e0eae6
Ravish021 <ravishgupta021@gmail.com>
2024-10-05 23:20:47 +0530

    fix core origin issue

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json({limit:'50mb'}));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
+    origin:'*',
     credentials:true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization'
