7adf1eef15bb36e29e2de58014c1c917208d8c30
Ravish021 <ravishgupta021@gmail.com>
2024-11-23 21:52:57 +0530

    use url to fix

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json({limit:'50mb'}));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-    origin:'*',
+    origin:'https://ecommerce-three-tawny-53.vercel.app/',
     credentials:true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization'
